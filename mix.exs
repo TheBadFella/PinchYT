20f610293d371c23643874bbeadc1752da212d51
TheBadFella <chashamnoor1@gmail.com>
2026-08-14 19:00:45 -0600

    chore(release): bump version to 2026.8.14

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Pinchflat.MixProject do
   def project do
     [
       app: :pinchflat,
-      version: "2026.7.2",
+      version: "2026.8.14",
       elixir: "~> 1.17",
       elixirc_paths: elixirc_paths(Mix.env()),
       elixirc_options: [warnings_as_errors: System.get_env("EX_CHECK") == "1"],
